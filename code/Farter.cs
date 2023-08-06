048d88b11307cc0faa84fa0f62f2d128886275b1
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2023-08-06 20:43:35 +0300

    exceptions

--- code/Farter.cs
+++ code/Farter.cs
@@ -16,9 +16,10 @@ public class Farter
         }
     }
 
-    private static RangedFloat _micSpamInterval = new(60f, 120f);
+    private static RangedFloat _micSpamInterval = new(30f, 60f);
     private static RangedFloat _gaslightInterval = new(20f, 60f);
     private static RangedFloat _inconvenientRockInterval = new(20, 60);
+    private static RangedFloat _fakeExceptionInterval = new(120, 360);
 
     private static Farter _the;
 
@@ -31,6 +32,8 @@ public class Farter
     private float NextGaslight = _gaslightInterval.GetValue();
     private TimeSince LastInconvenientRock = 0;
     private float NextInconvenientRock = _inconvenientRockInterval.GetValue();
+    private TimeSince LastFakeException = 0;
+    private float NextFakeException = _fakeExceptionInterval.GetValue();
 
     private Farter()
     {
@@ -56,6 +59,12 @@ public class Farter
             if (Playlist.Count == 0)
             {
                 PopulatePlaylist();
+            }
+
+            if (Playlist.Count == 0) // still no sounds?
+            {
+                Log.Error("no sounds?");
+                return;
             }
 
             var s = Playlist.Dequeue();
@@ -152,10 +161,31 @@ public class Farter
             LastInconvenientRock = 0;
             NextInconvenientRock = _inconvenientRockInterval.GetValue();
         }
+
+        if (LastFakeException > NextFakeException)
+        {
+            Log.Trace(new Exception("Null Reference Exception") { Source = "code/HL2Combine.cs:27" });
+
+            LastFakeException = 0;
+            NextFakeException = _fakeExceptionInterval.GetValue();
+        }
     }
 
     private void PopulatePlaylist()
     {
         Playlist = new Queue<MicSpamAsset>(MicSpamAsset.All.OrderBy(a => Guid.NewGuid()));
+/*
+        foreach (var p in Playlist)
+            Log.Info($"{p.SoundEvent}");*/
     }
+
+    /*
+    [ConCmd.Admin("trigger_all")]
+    private static void TriggerAll()
+    {
+        The.LastFakeException = 1000;
+        The.LastGaslight = 1000;
+        The.LastInconvenientRock = 1000;
+        The.LastMicSpam = 1000;
+    }*/
 }
